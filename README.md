377b9a5903c8317f4ca717335dca29a9a01a0b94
stkasuga <stkasuga@gmail.com>
2024-12-17 16:25:18 +0000

    add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositorio para usar no curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Isso eh muito legal</h1>
+  return <h1>Isso eh muito legal</h1>;
 }
 
 export default Home;
